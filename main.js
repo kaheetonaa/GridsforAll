1cbe55da2b20b4564d361021b98f189043d18b2d
Quang Huy <kaheetonaa@gmail.com>
2025-03-10 17:06:22 +0100

    update

--- main.js
+++ main.js
@@ -31,14 +31,6 @@ const raster = new RasterSource({
     }),
   ],
   operation: function (pixels) {
-    const Xn = 0.95047;
-    const Yn = 1;
-    const Zn = 1.08883;
-    const t0 = 4 / 29;
-    const t1 = 6 / 29;
-    const t2 = 3 * t1 * t1;
-    const t3 = t1 * t1 * t1;
-    const twoPi = 2 * Math.PI;
     function invert_color(pixel) {
       const red = 255 - pixel[0];
       const green = 255 - pixel[1];
@@ -50,41 +42,41 @@ const raster = new RasterSource({
     }
 
     function rgb2hsl(pixel) {
-      let r = pixel[0]/255;
-      let g = pixel[1]/255;
-      let b = pixel[2]/255;
+      let r = pixel[0] / 255;
+      let g = pixel[1] / 255;
+      let b = pixel[2] / 255;
 
       var max = Math.max(r, g, b);
-    var min = Math.min(r, g, b);
-    // lightness is the average of the largest and smallest color components
-    var lum = (max + min) / 2;
-    var hue;
-    var sat;
-    if (max == min) { // no saturation
+      var min = Math.min(r, g, b);
+      // lightness is the average of the largest and smallest color components
+      var lum = (max + min) / 2;
+      var hue;
+      var sat;
+      if (max == min) { // no saturation
         hue = 0;
         sat = 0;
-    } else {
+      } else {
         var c = max - min; // chroma
         // saturation is simply the chroma scaled to fill
         // the interval [0, 1] for every combination of hue and lightness
         sat = c / (1 - Math.abs(2 * lum - 1));
-        switch(max) {
-            case r:
-                // hue = (g - b) / c;
-                // hue = ((g - b) / c) % 6;
-                // hue = (g - b) / c + (g < b ? 6 : 0);
-                break;
-            case g:
-                hue = (b - r) / c + 2;
-                break;
-            case b:
-                hue = (r - g) / c + 4;
-                break;
+        switch (max) {
+          case r:
+            // hue = (g - b) / c;
+            // hue = ((g - b) / c) % 6;
+            // hue = (g - b) / c + (g < b ? 6 : 0);
+            break;
+          case g:
+            hue = (b - r) / c + 2;
+            break;
+          case b:
+            hue = (r - g) / c + 4;
+            break;
         }
-    }
-    let h = Math.round(hue * 60); // °
-    let s = Math.round(sat * 100); // %
-    let l = Math.round(lum * 100); // %
+      }
+      let h = Math.round(hue * 60); // °
+      let s = Math.round(sat * 100); // %
+      let l = Math.round(lum * 100); // %
 
       pixel[0] = h;
       pixel[1] = s;
@@ -93,81 +85,74 @@ const raster = new RasterSource({
       return pixel;
     }
 
-    /**
-     * Convert an HCL pixel into an RGB pixel.
-     * @param {Array<number>} pixel A pixel in HCL space.
-     * @return {Array<number>} A pixel in RGB space.
-     */
-    function hsl2rgb (pixel) {
-      h=pixel[0];
-      s=pixel[1];
-      l=pixel[2];
+    function hsl2rgb(pixel) {
+      h = pixel[0];
+      s = pixel[1];
+      l = pixel[2];
 
       let r, g, b, m, c, x
-  
+
       if (!isFinite(h)) h = 0
       if (!isFinite(s)) s = 0
       if (!isFinite(l)) l = 0
-  
+
       h /= 60
       if (h < 0) h = 6 - (-h % 6)
       h %= 6
-  
+
       s = Math.max(0, Math.min(1, s / 100))
       l = Math.max(0, Math.min(1, l / 100))
-  
+
       c = (1 - Math.abs((2 * l) - 1)) * s
       x = c * (1 - Math.abs((h % 2) - 1))
-  
+
       if (h < 1) {
-          r = c
-          g = x
-          b = 0
+        r = c
+        g = x
+        b = 0
       } else if (h < 2) {
-          r = x
-          g = c
-          b = 0
+        r = x
+        g = c
+        b = 0
       } else if (h < 3) {
-          r = 0
-          g = c
-          b = x
+        r = 0
+        g = c
+        b = x
       } else if (h < 4) {
-          r = 0
-          g = x
-          b = c
+        r = 0
+        g = x
+        b = c
       } else if (h < 5) {
-          r = x
-          g = 0
-          b = c
+        r = x
+        g = 0
+        b = c
       } else {
-          r = c
-          g = 0
-          b = x
+        r = c
+        g = 0
+        b = x
       }
-  
+
       m = l - c / 2
       pixel[0] = Math.round((r + m) * 255)
       pixel[1] = Math.round((g + m) * 255)
       pixel[2] = Math.round((b + m) * 255)
       return pixel;
-
-  
-  }
+    }
 
     const hsl = rgb2hsl(pixels[0]);
     hsl[1] *= 0;
     hsl[2] *= 1;
 
     return invert_color(hsl2rgb(hsl));
+    
   },
 });
-
 
 const map = new Map({
   layers: [
     new ImageLayer({
       source: raster,
-    }),
+    })
   ],
   target: 'map',
   view: new View({
@@ -185,28 +170,75 @@ let grid_style = new Style({
     src: cross,
   }),
 })
-let grid_size_list=[250000,50000,5000,1000]
-let grid_size=grid_size_list[0]
-let grid=new Grid({ originCoordinate: [0, 0], rotationAnchorCoordinate: [0, 1], xGridSize: grid_size, yGridSize: grid_size, style: grid_style });
+let grid_size_list = [250000, 50000, 5000, 1000]
+let grid_size = grid_size_list[0]
+let grid = new Grid({ originCoordinate: [0, 0], rotationAnchorCoordinate: [0, 1], xGridSize: grid_size, yGridSize: grid_size, style: grid_style });
 map.addInteraction(grid)
+
+raster.on('beforeoperations',() => {
+  console.log('Loading tiles,might take times!')
+})
+raster.on('afteroperations',() => {
+  console.log('Done loading tiles!')
+})
+
 map.on('moveend', (e) => {
-  let zoom= map.getView().getZoom();
-  if (zoom<=7){
-    grid_size=grid_size_list[0]
+  let zoom = map.getView().getZoom();
+  if (zoom <= 7) {
+    grid_size = grid_size_list[0]
   }
-  if (zoom<=10 && zoom>7) {
-    grid_size=grid_size_list[1]
+  if (zoom <= 10 && zoom > 7) {
+    grid_size = grid_size_list[1]
   }
-  if (zoom<=13 && zoom>10) {
-    grid_size=grid_size_list[2]
+  if (zoom <= 13 && zoom > 10) {
+    grid_size = grid_size_list[2]
   }
-  if (zoom>13) {
-    grid_size=grid_size_list[3]
+  if (zoom > 13) {
+    grid_size = grid_size_list[3]
   }
   grid.setXGridSize(grid_size)
   grid.setYGridSize(grid_size)
-  console.log(zoom,grid.xGridSize)
+
 })
+dragElement(document.getElementById("draggableDiv"));
 
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if (document.getElementById(elmnt.id + "header")) {
+    // if present, the header is where you move the DIV from:
+    document.getElementById(elmnt.id + "header").onpointerdown = dragpointerDown;
+  } else {
+    // otherwise, move the DIV from anywhere inside the DIV:
+    elmnt.onpointerdown = dragpointerDown;
+  }
 
+  function dragpointerDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the pointer cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onpointerup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onpointermove = elementDrag;
+  }
 
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    // stop moving when pointer button is released:
+    document.onpointerup = null;
+    document.onpointermove = null;
+  }
+}
