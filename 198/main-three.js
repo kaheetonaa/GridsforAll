8b8884a308233534f8d7d89d787d9a188c9b9b1d
kaheetonaa <61049296+kaheetonaa@users.noreply.github.com>
2025-04-20 00:05:11 +0200

    camera.zoom

--- 198/main-three.js
+++ 198/main-three.js
@@ -7,6 +7,7 @@ init();
 function init() {
     scene = new THREE.Scene(); //draw scene
     camera = new THREE.OrthographicCamera( window.innerWidth / - 2, window.innerWidth / 2, window.innerHeight / 2, window.innerHeight / - 2, 1, 1000 )
+    camera.zoom=.001; //test 
 
     renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
